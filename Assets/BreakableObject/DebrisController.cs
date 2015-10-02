1e5f050968c2f74ad36f714d6a4d910e975b56d9
PSimonBP <psimonbp@gmail.com>
2015-10-02 10:56:48 +0200

    Fix

--- Assets/BreakableObject/DebrisController.cs
+++ Assets/BreakableObject/DebrisController.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class DebrisController : MonoBehaviour {
 
-	private Rigidbody2D	tRigidBody = null;
-	private BreakableBox tBreakableBox = null;
+	private Rigidbody2D		tRigidBody = null;
+	private BreakableBox	tBreakableBox = null;
 	private float			fEndTime = 0;
+	private int				iOriginalLayer = 0;
 
 	// Use this for initialization
 	void Start () {
+		iOriginalLayer = gameObject.layer;
 		gameObject.layer = 8;
 		tRigidBody = gameObject.GetComponent<Rigidbody2D>();
 		tBreakableBox = gameObject.GetComponent<BreakableBox>();
@@ -18,6 +20,7 @@ public class DebrisController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (fEndTime < Time.time || tRigidBody.IsSleeping()) {
+			gameObject.layer = iOriginalLayer;
 			tBreakableBox.Deactivate();
 			Destroy(this);
 		}
