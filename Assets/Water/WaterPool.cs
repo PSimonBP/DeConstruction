91307b6fe881c0de30ddd2af2336b772c0bfa7e3
PSimonBP <psimonbp@gmail.com>
2015-10-16 18:19:04 +0200

    Water rendering update

--- Assets/Water/WaterPool.cs
+++ Assets/Water/WaterPool.cs
@@ -5,21 +5,18 @@ public class WaterPool : Container
 	static WaterPool			_instance;
 	public static WaterPool		Instance { get { return _instance; } }
 
-	void Start ()
+	void Start()
 	{
 		_instance = this;
-		Setup (typeof(WaterController));
+		Setup(typeof(WaterController));
 	}
 
-	void Update ()
+	void Update()
 	{
-//		if (Random.Range (0, 10) > 3) 
-		{
-			GameObject tDrop = WaterPool.Instance.GetObject ();
-			if (tDrop != null) {
-				tDrop.transform.position = transform.position;
-				tDrop.transform.parent = transform;
-			}			
-		}
+		GameObject tDrop = WaterPool.Instance.GetObject();
+		if (tDrop != null) {
+			tDrop.transform.position = transform.position;
+			tDrop.transform.parent = transform;
+		}			
 	}
 }
