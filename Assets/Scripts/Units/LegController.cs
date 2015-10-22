1779cda8713ada9c4cb505f76a7fdff9e221a01f
PSimonBP <psimonbp@gmail.com>
2015-10-22 23:16:46 +0200

    Mech

--- Assets/Scripts/Units/LegController.cs
+++ Assets/Scripts/Units/LegController.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LegController : MonoBehaviour {
+public class LegController : MonoBehaviour
+{
 	public float	StepSize = 0.05f;
 	public float	StepTime = 1.0f;
 
@@ -20,11 +21,12 @@ public class LegController : MonoBehaviour {
 	Vector2			m_tMoveSpeed = Vector2.zero;
 	float			m_fTurnSpeed = 0;
 	
-	void Start () {
+	void Start ()
+	{
 		m_fCurrentStepTime = StepTime / 2.0f;
 		
-		m_tController = gameObject.GetComponentInParent<UnitController>();
-		Transform[] tChildren = gameObject.GetComponentsInChildren<Transform>();
+		m_tController = gameObject.GetComponentInParent<UnitController> ();
+		Transform[] tChildren = gameObject.GetComponentsInChildren<Transform> ();
 		foreach (Transform tChild in tChildren) {
 			if (tChild.gameObject.name == "Leg_L")
 				m_tLeftLeg = tChild.gameObject;
@@ -37,45 +39,81 @@ public class LegController : MonoBehaviour {
 		m_tRightDefPos = m_tRightLeg.transform.localPosition;
 		m_fRightDefRot = m_tRightLeg.transform.localRotation.z;
 
-		if (Random.Range(0, 10) >= 5)
+		if (Random.Range (0, 10) >= 5)
 			m_bLeftLegMovingFWD = true;
-		if (Random.Range(0, 10) >= 5)
+		if (Random.Range (0, 10) >= 5)
 			m_bLeftLegTurning = true;
 	}
 	
-	void Update () {
-		UnitController.EMovementStates tMovement = m_tController.GetCurrentMovement();
+	void Update ()
+	{
+		UnitController.EMovementStates tMovement = m_tController.GetCurrentMovement ();
 		float fWalkSpeed = 0.0f;
 		float fSideSpeed = 0.0f;
 		float fTurnSpeed = 0.0f;
 		switch (tMovement) {
-			case UnitController.EMovementStates.MS_WALK_FWD:	fWalkSpeed = 1.0f;	break;
-			case UnitController.EMovementStates.MS_WALK_FWD_L:	fWalkSpeed = 1.0f;	fTurnSpeed = 10.0f;	break;
-			case UnitController.EMovementStates.MS_WALK_FWD_R:	fWalkSpeed = 1.0f;	fTurnSpeed = -10.0f;	break;
-			case UnitController.EMovementStates.MS_RUN_FWD:		fWalkSpeed = 1.6f;	break;
-			case UnitController.EMovementStates.MS_RUN_FWD_L:	fWalkSpeed = 1.6f;	fTurnSpeed = 10.0f;	break;
-			case UnitController.EMovementStates.MS_RUN_FWD_R:	fWalkSpeed = 1.6f;	fTurnSpeed = -10.0f;	break;
-			case UnitController.EMovementStates.MS_WALK_BWD:	fWalkSpeed = -0.6f;	break;
-			case UnitController.EMovementStates.MS_WALK_BWD_L:	fWalkSpeed = -0.6f;	fTurnSpeed = 10.0f;	break;
-			case UnitController.EMovementStates.MS_WALK_BWD_R:	fWalkSpeed = -0.6f;	fTurnSpeed = -10.0f;	break;
-			case UnitController.EMovementStates.MS_WALK_LEFT:	fSideSpeed = -0.6f;	break;
-			case UnitController.EMovementStates.MS_WALK_RIGHT:	fSideSpeed = 0.6f;	break;
-			case UnitController.EMovementStates.MS_TURN_L:		fTurnSpeed = 10.0f;	break;
-			case UnitController.EMovementStates.MS_TURN_R:		fTurnSpeed = -10.0f;	break;
+		case UnitController.EMovementStates.MS_WALK_FWD:
+			fWalkSpeed = 1.0f;
+			break;
+		case UnitController.EMovementStates.MS_WALK_FWD_L:
+			fWalkSpeed = 1.0f;
+			fTurnSpeed = 10.0f;
+			break;
+		case UnitController.EMovementStates.MS_WALK_FWD_R:
+			fWalkSpeed = 1.0f;
+			fTurnSpeed = -10.0f;
+			break;
+		case UnitController.EMovementStates.MS_RUN_FWD:
+			fWalkSpeed = 1.6f;
+			break;
+		case UnitController.EMovementStates.MS_RUN_FWD_L:
+			fWalkSpeed = 1.6f;
+			fTurnSpeed = 10.0f;
+			break;
+		case UnitController.EMovementStates.MS_RUN_FWD_R:
+			fWalkSpeed = 1.6f;
+			fTurnSpeed = -10.0f;
+			break;
+		case UnitController.EMovementStates.MS_WALK_BWD:
+			fWalkSpeed = -0.6f;
+			break;
+		case UnitController.EMovementStates.MS_WALK_BWD_L:
+			fWalkSpeed = -0.6f;
+			fTurnSpeed = 10.0f;
+			break;
+		case UnitController.EMovementStates.MS_WALK_BWD_R:
+			fWalkSpeed = -0.6f;
+			fTurnSpeed = -10.0f;
+			break;
+		case UnitController.EMovementStates.MS_WALK_LEFT:
+			fSideSpeed = -0.6f;
+			break;
+		case UnitController.EMovementStates.MS_WALK_RIGHT:
+			fSideSpeed = 0.6f;
+			break;
+		case UnitController.EMovementStates.MS_TURN_L:
+			fTurnSpeed = 10.0f;
+			break;
+		case UnitController.EMovementStates.MS_TURN_R:
+			fTurnSpeed = -10.0f;
+			break;
 		}
 		
-		m_tMoveSpeed = new Vector2(SideStep(fSideSpeed), Step(fWalkSpeed));
-		m_fTurnSpeed = Turn(fTurnSpeed);
+		m_tMoveSpeed = new Vector2 (SideStep (fSideSpeed), Step (fWalkSpeed));
+		m_fTurnSpeed = Turn (fTurnSpeed);
 	}
 
-	void FixedUpdate() {
+	void FixedUpdate ()
+	{
 //		m_tController.rigidbody2D.AddTorque(m_fTurnSpeed * Time.fixedDeltaTime, ForceMode2D.Force);
 //		m_tController.rigidbody2D.AddRelativeForce(m_tMoveSpeed * 3, ForceMode2D.Force);
-		m_tController.GetComponent<Rigidbody2D>().velocity = m_tController.GetComponent<Rigidbody2D>().GetRelativeVector(m_tMoveSpeed);
-		m_tController.GetComponent<Rigidbody2D>().angularVelocity = m_fTurnSpeed * 6;
+		Rigidbody2D tBody = m_tController.GetComponent<Rigidbody2D> ();
+		tBody.velocity = tBody.GetRelativeVector (m_tMoveSpeed) * tBody.transform.localScale.x * 0.75f;
+		tBody.angularVelocity = m_fTurnSpeed * 6;
 	}
 
-	protected virtual float Step(float fWalkSpeed) {
+	protected virtual float Step (float fWalkSpeed)
+	{
 		if (fWalkSpeed == 0)
 			return 0;
 		m_fCurrentStepTime += Time.deltaTime * fWalkSpeed * (m_bLeftLegMovingFWD ? 1.0f : -1.0f);
@@ -86,22 +124,24 @@ public class LegController : MonoBehaviour {
 			m_fCurrentStepTime = 0;
 			m_bLeftLegMovingFWD = !m_bLeftLegMovingFWD;
 		}
-		float fSin = Mathf.Sin((Mathf.PI * 2.0f) * (m_fCurrentStepTime / StepTime)) * (m_bLeftLegMovingFWD ? 1 : -1);
+		float fSin = Mathf.Sin ((Mathf.PI * 2.0f) * (m_fCurrentStepTime / StepTime)) * (m_bLeftLegMovingFWD ? 1 : -1);
 		Vector3 tPos = m_tLeftDefPos;
 		Vector3 tRPos = m_tRightDefPos;
 		tPos.y += StepSize * fSin;
 		tRPos.y -= StepSize * fSin;
 		m_tLeftLeg.transform.localPosition = tPos;
 		m_tRightLeg.transform.localPosition = tRPos;
-		return fWalkSpeed * Mathf.Abs(fSin); // todo precise
+		return fWalkSpeed * Mathf.Abs (fSin); // todo precise
 	}
 
-	protected virtual float SideStep(float fSideSpeed) {
+	protected virtual float SideStep (float fSideSpeed)
+	{
 		// todo
 		return 0;
 	}
 
-	protected virtual float Turn(float fTurnSpeed) {
+	protected virtual float Turn (float fTurnSpeed)
+	{
 		float fAngle = fTurnSpeed;
 		if (fTurnSpeed != 0 || m_tLeftLeg.transform.localRotation.z != m_fLeftDefRot) {
 			float fTrnSpeed = Time.deltaTime * fTurnSpeed;
@@ -113,7 +153,7 @@ public class LegController : MonoBehaviour {
 					tRot.z += Time.deltaTime / 5.0f;
 				else
 					tRot.z -= Time.deltaTime / 5.0f;
-				if (Mathf.Abs(tRot.z) < 0.001f)
+				if (Mathf.Abs (tRot.z) < 0.001f)
 					tRot.z = 0;
 			} else {
 				tRot.z += fTrnSpeed / (m_bLeftLegTurning ? 50.0f : -50.0f);
--- Assets/Scripts/Units/WeaponController.cs
+++ Assets/Scripts/Units/WeaponController.cs
@@ -1,32 +1,32 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class WeaponController : MonoBehaviour
 {
-	public enum EWeaponType
+/*	public enum EWeaponType
 	{
 		WT_BULLET,
 		WT_ROCKET,
 		WT_LASER,
 		WT_FLAME
 	}
-	public float		RateOfFire = 1.0f;
-	public float		Strength = 1.0f;
-	public EWeaponType	WeaponType = EWeaponType.WT_BULLET;
+*/
+	public float		RateOfFire = 1;
+	public int			Strength = 1;
+//	public EWeaponType	WeaponType = EWeaponType.WT_BULLET;
 
-	UnitController		m_tController = null;
-	BodyController		m_tBody = null;
-	float				m_fTimeToReady = 0.0f;
+	UnitController		m_tController;
+	BodyController		m_tBody;
+	float				m_fTimeToReady;
 	float				m_fEffectTime = float.MaxValue;
 
-	Renderer			m_tFireEffect1 = null;
-	Renderer			m_tFireEffect2 = null;
+	Renderer			m_tFireEffect1;
+	Renderer			m_tFireEffect2;
 
-	void Start()
+	void Start ()
 	{
-		m_tController = gameObject.GetComponentInParent<UnitController>();
-		m_tBody = gameObject.GetComponentInParent<BodyController>();
-		Renderer[] tFireEffects = gameObject.GetComponentsInChildren<Renderer>();
+		m_tController = gameObject.GetComponentInParent<UnitController> ();
+		m_tBody = gameObject.GetComponentInParent<BodyController> ();
+		Renderer[] tFireEffects = gameObject.GetComponentsInChildren<Renderer> ();
 		foreach (Renderer tEffect in tFireEffects) {
 			if (tEffect.name == "Fire 1")
 				m_tFireEffect1 = tEffect;
@@ -35,18 +35,24 @@ public class WeaponController : MonoBehaviour
 		}
 	}
 	
-	void Update()
+	void Update ()
 	{
-		if (m_tController.IsAttacking()) {
+		if (m_tController.IsAttacking ()) {
 			if (m_fTimeToReady <= 0) {
-				for (int i=0; i<10; ++i) {
-					GameObject tDrop = WaterPool.Instance.GetObject();
+				for (int i=0; i<Strength; ++i) {
+					GameObject tDrop = WaterPool.Instance.GetObject ();
 					if (tDrop != null) {
-						Vector3 tPosition = transform.position + new Vector3(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f), 0);
+						Vector3 tPosition = transform.position + new Vector3 (Random.Range (-0.1f, 0.1f), Random.Range (-0.1f, 0.1f), 0);
 						tPosition.z = 0;
 						tDrop.transform.position = tPosition;
-						tDrop.transform.SetParent(WaterPool.Instance.transform);
-						tDrop.GetComponent<Rigidbody2D>().velocity = new Vector2(m_tBody.transform.rotation.eulerAngles.normalized.z, m_tBody.transform.rotation.eulerAngles.normalized.x) * 20;
+						tDrop.transform.SetParent (WaterPool.Instance.transform);
+
+						float fAngle = Random.Range (-20, 20);
+						Quaternion tNewRot = Quaternion.AngleAxis (fAngle, Vector3.up);
+						tDrop.transform.rotation = Quaternion.RotateTowards (m_tBody.transform.rotation, tNewRot, fAngle);
+						tDrop.GetComponent<Rigidbody2D> ().AddRelativeForce (new Vector2 (0, 1000));
+
+//						tDrop.GetComponent<Rigidbody2D>().velocity = new Vector2(m_tBody.transform.rotation.eulerAngles.normalized.z, m_tBody.transform.rotation.eulerAngles.normalized.x) * 20;
 					}
 				}
 
@@ -64,16 +70,16 @@ public class WeaponController : MonoBehaviour
 		float fEffectPartTime = 0.02f;
 		if (m_fEffectTime <= fEffectPartTime * 5) {
 			if (m_fEffectTime > fEffectPartTime && m_fEffectTime < fEffectPartTime * 4) {
-				m_tFireEffect1.GetComponent<Renderer>().enabled = (m_fEffectTime > fEffectPartTime * 2 && m_fEffectTime < fEffectPartTime * 3);
-				m_tFireEffect2.GetComponent<Renderer>().enabled = true;
+				m_tFireEffect1.GetComponent<Renderer> ().enabled = (m_fEffectTime > fEffectPartTime * 2 && m_fEffectTime < fEffectPartTime * 3);
+				m_tFireEffect2.GetComponent<Renderer> ().enabled = true;
 			} else {
-				m_tFireEffect1.GetComponent<Renderer>().enabled = true;
-				m_tFireEffect2.GetComponent<Renderer>().enabled = false;
+				m_tFireEffect1.GetComponent<Renderer> ().enabled = true;
+				m_tFireEffect2.GetComponent<Renderer> ().enabled = false;
 			}
 			m_fEffectTime += Time.deltaTime;
 		} else {
-			m_tFireEffect1.GetComponent<Renderer>().enabled = false;
-			m_tFireEffect2.GetComponent<Renderer>().enabled = false;
+			m_tFireEffect1.GetComponent<Renderer> ().enabled = false;
+			m_tFireEffect2.GetComponent<Renderer> ().enabled = false;
 		}
 	}
 }
