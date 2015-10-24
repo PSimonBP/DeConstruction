c8de38627d0acd9dcf2510e598e824548e6150d6
PSimonBP <psimonbp@gmail.com>
2015-10-24 04:39:26 +0200

    fixes

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -4,30 +4,32 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class GameLogic : MonoBehaviour
 {
-	int m_iUpdateSteps;
-	int m_iDefaultPosIter;
-	int m_iDepaultVelIter;
+//	int m_iUpdateSteps;
+//	int m_iDefaultPosIter;
+//	int m_iDepaultVelIter;
 //	Vector2 InitialGravity = Vector2.zero;
 
-	void Start()
+	void Start ()
 	{
-		m_iDefaultPosIter = Physics2D.positionIterations;
-		m_iDepaultVelIter = Physics2D.velocityIterations;
+//		m_iDefaultPosIter = Physics2D.positionIterations;
+//		m_iDepaultVelIter = Physics2D.velocityIterations;
 //		InitialGravity = Physics2D.gravity;
 //		Input.gyro.enabled = true;
 	}
 
 	
-	void Update()
+	void Update ()
 	{
-		if (Input.GetKeyDown(KeyCode.T) || (CrossPlatformInputManager.ButtonExists("SlowMotion") && CrossPlatformInputManager.GetButtonDown("SlowMotion"))) {
-			if (System.Math.Abs(Time.timeScale - 1) < Mathf.Epsilon)
+		if (Input.GetKeyDown (KeyCode.T) || (CrossPlatformInputManager.ButtonExists ("SlowMotion") && CrossPlatformInputManager.GetButtonDown ("SlowMotion"))) {
+			if (System.Math.Abs (Time.timeScale - 1) < Mathf.Epsilon)
 				Time.timeScale = 0.2f;
 			else
 				Time.timeScale = 1;
+			return;
 		}
-		if (Input.GetKeyDown(KeyCode.R) || (CrossPlatformInputManager.ButtonExists("Reset") && CrossPlatformInputManager.GetButtonDown("Reset"))) {
-			Application.LoadLevel("MainScene");
+		if (Input.GetKeyDown (KeyCode.R) || (CrossPlatformInputManager.ButtonExists ("Reset") && CrossPlatformInputManager.GetButtonDown ("Reset"))) {
+			Application.LoadLevel ("MainScene");
+			return;
 		}
 
 		if (Input.touchCount > 0) {
@@ -35,43 +37,10 @@ public class GameLogic : MonoBehaviour
 			for (int i = 0; i < Input.touchCount; i++) {
 //				Fire(Camera.main.ScreenToWorldPoint(tTouches [i].position));
 			}
-		} else {
-			if (Input.GetMouseButton(0)) {
-//				Fire(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-			}
-			if (Input.GetMouseButton(1)) {
-				RaycastHit2D tHit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-				if (tHit.collider != null) {
-					BreakableBox tBox = tHit.collider.GetComponent<BreakableBox>();
-					if (tBox)
-						tBox.Break();
-				}
-			}
 		}
-
-/*		if (Input.gyro.enabled) {
-			var tGrav = Input.gyro.gravity - new Vector2(Input.acceleration.x, Input.acceleration.y);
-			Physics2D.gravity = InitialGravity.magnitude * new Vector2(tGrav.x, tGrav.y);
-		}
-*/
 	}
 
-	void Fire(Vector3 tStart)
-	{
-/*		for (int i=0; i<10; ++i) {
-			GameObject tDrop = WaterPool.Instance.GetObject();
-			if (tDrop != null) {
-				Vector3 tPosition = tStart + new Vector3(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f), 0);
-				tPosition.z = 0;
-				tDrop.transform.position = tPosition;
-				tDrop.transform.SetParent(WaterPool.Instance.transform);
-				tDrop.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(10, 20), Random.Range(-0.1f, 0.1f));
-			}
-		}
-*/
-	}
-	
-	void FixedUpdate()
+/*	void FixedUpdate()
 	{
 		if (++m_iUpdateSteps % 12 == 0) {
 			Physics2D.positionIterations = m_iDefaultPosIter;
@@ -81,4 +50,5 @@ public class GameLogic : MonoBehaviour
 			Physics2D.velocityIterations = 1;
 		}
 	}
+*/
 }
--- Assets/Water/WaterController.cs
+++ Assets/Water/WaterController.cs
@@ -2,47 +2,48 @@
 
 public class WaterController : MonoBehaviour
 {
-//	Rigidbody2D tRigidBody;
+	public Rigidbody2D RigidBody { get; set; }
 	float Life;
+	int SplitCount;
 	public float MaxLife { get; set; }
 	public bool Split { get; set; }
 
-//	Component m_tLight;
-//	Color m_tStartColor;
-//	Color m_tEndColor = new Color (1, 0.266f, 0, 1);
 	void Start ()
 	{
-//		m_tLight = GetComponent ("Halo");
-//		m_tStartColor = m_tLight.color;
+		Init ();
 	}
 
 	void OnEnable ()
 	{
+		Init ();
+	}
+
+	public void Init ()
+	{
 		Split = true;
+		SplitCount = 1;
 		Life = 0;
 		MaxLife = WaterPool.Instance.LifeTime * (Random.Range (0.8f, 1.2f));
+		RigidBody = GetComponent<Rigidbody2D> ();
 	}
 
 	void Update ()
 	{
 		Life += Time.deltaTime;
-//		float fRatio = Life / MaxLife;
-//		m_tLight.color = new Color ((m_tStartColor.r * 1 - fRatio) + (m_tEndColor.r * fRatio), (m_tStartColor.g * 1 - fRatio) + (m_tEndColor.g * fRatio), (m_tStartColor.b * 1 - fRatio) + (m_tEndColor.b * fRatio), 255);
-		if (Life >= MaxLife * 0.05f && Life < MaxLife * 0.95f && Split) {
-			if (Random.Range (0, 100) > 95) {
-				var tObj = WaterPool.Instance.GetObject ();
-				if (tObj) {
-					var tNewObj = tObj.GetComponent<WaterController> ();
-					tNewObj.transform.SetParent (transform.parent);
-					tNewObj.transform.localPosition = transform.localPosition;
-					tNewObj.transform.localRotation = transform.localRotation;
-					Vector2 tVel = GetComponent<Rigidbody2D> ().velocity;
-					tVel.x *= Random.Range (0.8f, 1.2f);
-					tVel.y *= Random.Range (0.8f, 1.2f);
-					tNewObj.GetComponent<Rigidbody2D> ().velocity = tVel;
-					tNewObj.MaxLife = (MaxLife - Life) * Random.Range (0.8f, 1.2f);
-					tNewObj.Split = false;
-				}
+		if (Split && SplitCount > 0 && Life >= MaxLife / (SplitCount + 1)) {
+			SplitCount--;
+			var tObj = WaterPool.Instance.GetObject ();
+			if (tObj) {
+				var tNewObj = tObj.GetComponent<WaterController> ();
+				tNewObj.transform.SetParent (transform.parent);
+				tNewObj.transform.localPosition = transform.localPosition;
+				tNewObj.transform.localRotation = transform.localRotation;
+				tNewObj.Init ();
+				Vector2 tVel = RigidBody.velocity;
+				tVel.x += Random.Range (-3, 3);
+				tVel.y += Random.Range (-3, 3);
+				tNewObj.RigidBody.velocity = tVel;
+				tNewObj.Split = false;
 			}
 		} else if (Life >= MaxLife) {
 			WaterPool.Instance.PoolObject (gameObject);
